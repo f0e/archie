7f97b6f888f036472f1afab7264a9a0fc23e02f2
f0e <7321764+f0e@users.noreply.github.com>
2023-08-28 03:41:07 +1000

    more parsing

--- sources/youtube.py
+++ sources/youtube.py
@@ -12,11 +12,7 @@ def debug_write(yt, data, filename):
         out_file.write(json.dumps(yt.sanitize_info(data)))
 
 
-def get_page(yt, channel, page):
-    return yt.extract_info(f'https://www.youtube.com/{channel}/{page}', download=False)
-
-
-def add_channel(channel):
+def parse_channel(channelLink):
     # adds a channel to the database. will filter out unwanted channels.
     # only adds basic information on /about page
 
@@ -25,36 +21,51 @@ def add_channel(channel):
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as yt:
-        about = get_page(yt, channel, "about")
+        about = yt.extract_info(
+            f'https://www.youtube.com/{channelLink}/about', download=False)
+
         if filter.filter_about(about):
+            # todo: what to do when the channel's already been added
             return False
 
         # todo: do all channels have at least one avatar? this will fail if not
         avatar_data = download_image(about['thumbnails'][0]['url'])
 
-        Channel.create_or_update(
+        channel = Channel.create_or_update(
             id=about['channel_id'],
             name=about['channel'],
             avatar=avatar_data,
             description=about['description']
         )
 
+        parse_video_details(channel)
+
     return True
 
 
-def update_channel(channel):
+def parse_video_details(channel):
     # updates a channel's videos, playlists, etc.
 
     ydl_opts = {
+        # don't get videos, just get the data available from the /videos page
         'extract_flat': True,
         'quiet': True
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as yt:
-        videos = get_page(yt, channel, "videos")
+        videos = yt.extract_info(
+            f'https://www.youtube.com/channel/{channel.id}/videos', download=False)
+
         if filter.filter_videos(videos):
-            # todo: the channel's already been added but it's got too many videos or something
-            # What To Do
+            # todo: what to do when the channel's already been added
             pass
 
+        # channel.update_videos(videos)
+
+        # for video in videos parse_video_details
+
     return True
+
+
+def parse_video_details(video):
+    pass
