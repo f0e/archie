e737fd89a1448a1e807460b349202b9deb808570
f0e <7321764+f0e@users.noreply.github.com>
2023-09-01 16:32:58 +1000

    Revert "configging"

--- parse.py
+++ parse.py
@@ -3,7 +3,8 @@ from datetime import datetime, timedelta
 from database.database import Channel, ChannelStatus
 from sources import youtube
 
-from utils.config import settings
+CHANNEL_UPDATE_GAP_HOURS = 24
+VIDEO_UPDATE_GAP_HOURS = 24 * 7
 
 
 def log(message):
@@ -13,14 +14,14 @@ def log(message):
 def parse_accepted_channels():
     # goes through accepted channels videos
     while True:
-        channel = Channel.get_next_of_status(ChannelStatus.ACCEPTED, datetime.utcnow() - timedelta(hours=settings.channel_update_gap_hours))
+        channel = Channel.get_next_of_status(ChannelStatus.ACCEPTED, datetime.utcnow() - timedelta(hours=CHANNEL_UPDATE_GAP_HOURS))
         if not channel:
             break
 
         log(f"updating channel {channel.name} ({channel.id})")
         youtube.update_channel(channel)
 
-        video_min_update_time = datetime.utcnow() - timedelta(hours=settings.video_update_gap_hours)
+        video_min_update_time = datetime.utcnow() - timedelta(hours=VIDEO_UPDATE_GAP_HOURS)
 
         for video in channel.videos:
             if video.update_time > video_min_update_time:
--- sources/filter.py
+++ sources/filter.py
@@ -1,18 +1,24 @@
-from utils.config import settings
+MIN_SUBSCRIBERS = 0
+MAX_SUBSCRIBERS = 10000
+FILTER_VERIFIED = False
+
+BLOCK_NO_VIDEOS = False
+MAX_VIDEOS = 300
+FILTER_LIVESTREAMS = True
 
 
 def filter_channel_about(subscribers: int, verified: bool):
-    if subscribers > settings.max_subscribers or subscribers < settings.min_subscribers:
+    if subscribers > MAX_SUBSCRIBERS or subscribers < MIN_SUBSCRIBERS:
         return True
 
-    if verified and settings.filter_verified:
+    if verified and FILTER_VERIFIED:
         return True
 
     return False
 
 
 def filter_channel_videos(num_videos: int):
-    if (num_videos == 0 and settings.block_no_videos) or num_videos > settings.max_videos:
+    if (num_videos == 0 and BLOCK_NO_VIDEOS) or num_videos > MAX_VIDEOS:
         return True
 
     # todo: filter livestreams. is it possible here or does it have to be done after getting full video info
