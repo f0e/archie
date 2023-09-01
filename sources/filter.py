9dcf55a498512e258a3f62044ac5312c55d5716b
max <53710132+newgan@users.noreply.github.com>
2023-09-01 02:21:19 -0400

    update references to config items

--- sources/filter.py
+++ sources/filter.py
@@ -1,24 +1,18 @@
-MIN_SUBSCRIBERS = 0
-MAX_SUBSCRIBERS = 10000
-FILTER_VERIFIED = False
-
-BLOCK_NO_VIDEOS = False
-MAX_VIDEOS = 300
-FILTER_LIVESTREAMS = True
+from utils.config import settings
 
 
 def filter_channel_about(subscribers: int, verified: bool):
-    if subscribers > MAX_SUBSCRIBERS or subscribers < MIN_SUBSCRIBERS:
+    if subscribers > settings.max_subscribers or subscribers < settings.min_subscribers:
         return True
 
-    if verified and FILTER_VERIFIED:
+    if verified and settings.filter_verified:
         return True
 
     return False
 
 
 def filter_channel_videos(num_videos: int):
-    if (num_videos == 0 and BLOCK_NO_VIDEOS) or num_videos > MAX_VIDEOS:
+    if (num_videos == 0 and settings.block_no_videos) or num_videos > settings.max_videos:
         return True
 
     # todo: filter livestreams. is it possible here or does it have to be done after getting full video info
