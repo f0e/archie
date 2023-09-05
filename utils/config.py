5dd697152752119065a89d36431dabf38c5313ed
max <53710132+newgan@users.noreply.github.com>
2023-09-05 03:12:37 -0400

    add missing fields in config, remove unnecessary shit

--- utils/config.py
+++ utils/config.py
@@ -3,46 +3,58 @@ import json
 from pathlib import Path
 import os
 
-from dacite import from_dict
-
-from pydantic import TypeAdapter
-from pydantic.dataclasses import dataclass
+from pydantic import BaseModel, Field
 
 CFG_PATH = Path('config.json')
 
 
-@dataclass
-class Config():
-    archie_path: str = os.path.join(Path.home(), "archie")
+class Config(BaseModel):
+    archie_path: str = Field(default=os.path.join(Path.home(), "archie"))
 
-    min_subscribers: int = 0
-    max_subscribers: int = 30000
+    min_subscribers: int = Field(default=0)
+    max_subscribers: int = Field(default=30000)
 
-    filter_verified: bool = False
-    filter_livestreams: bool = True
+    filter_verified: bool = Field(default=False)
+    filter_livestreams: bool = Field(default=True)
 
-    block_no_videos: bool = False
-    max_videos: int = 300
+    block_no_videos: bool = Field(default=False)
+    max_videos: int = Field(default=300)
 
-    channel_update_gap_hours: int = 24
-    video_update_gap_hours: int = 24 * 7
+    channel_update_gap_hours: int = Field(default=24)
+    video_update_gap_hours: int = Field(default=24 * 7)
 
     def to_json(self):
-        return json.loads(TypeAdapter(Config).dump_json(self))
+        return json.loads(self.model_dump_json())
+
+    # add any missing keys from a config
+    def patch_keys(self, cfg_file: Path):
+        data = self.to_json()
+
+        with cfg_file.open('r+') as f:
+            cfg_data = json.load(f)
+
+            for key in data.keys():
+                value = data[key]
+
+                if key not in cfg_data:
+                    print(f'missing {key}')
+
+                    cfg_data[key] = value
+                    save_cfg(cfg_data)
 
 
-def save_cfg(cfg: Config):
+def save_cfg(data):
     with CFG_PATH.open('w') as f:
-        json.dump(cfg.to_json(), f, indent=2)
+        json.dump(data, f, indent=2)
 
 
 def load_cfg():
     if not CFG_PATH.exists() or CFG_PATH.stat().st_size == 0:
-        save_cfg(Config())
+        save_cfg(Config().to_json())
 
     with CFG_PATH.open('r') as f:
-        return from_dict(data_class=Config, data=json.load(f))
+        return Config().model_construct(f.read())
 
 
 settings = load_cfg()
-save_cfg(settings)
+settings.patch_keys(CFG_PATH)
